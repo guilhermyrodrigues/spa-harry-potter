eb6d4f4079610ed631736fabee0b0121e7dfab45
Guilhermy Rodrigues <guilhermyr87@gmail.com>
2025-03-15 20:09:21 -0300

    estruturacao do projeto

---
+++ src/app/characters/characters-detail/characters-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-characters-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './characters-detail.component.html',
+  styleUrl: './characters-detail.component.css'
+})
+export class CharactersDetailComponent {
+
+}
